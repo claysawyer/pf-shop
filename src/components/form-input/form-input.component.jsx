e374da62d66471dfb0e33ee1798b380db3f9d888
Clay Sawyer <claysawyer@outlook.com>
2023-04-19 12:48:32 -0500

    convert to styled components

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,27 +1,16 @@
-import React from "react";
-import "./form-input.styles.scss";
+import { FormInputLabel, Input, Group } from "./form-input.styles";
 
-// Define a functional component `FormInput` that takes in props, destructures
-// `label` and stores the rest of the props in an object `otherProps`
-const FormInput = ({ label, ...otherProps }) => (
-  // Render a div with a class "form-group"
-  <div className="form-group">
-    {/* Render an input with a class "form-input" and pass all the remaining
-    props to it */}
-    <input className="form-input" {...otherProps} />
-
-    {/* If a `label` prop is passed, render a label with class "form-input-label"
-    and a dynamic class "shrink" if there is a value in the input */}
-    {label && (
-      <label
-        className={`${
-          otherProps.value.length ? "shrink" : ""
-        } form-input-label`}
-      >
-        {label}
-      </label>
-    )}
-  </div>
-);
+const FormInput = ({ label, ...otherProps }) => {
+  return (
+    <Group>
+      <Input {...otherProps} />
+      {label && (
+        <FormInputLabel shrink={otherProps.value.length}>
+          {label}
+        </FormInputLabel>
+      )}
+    </Group>
+  );
+};
 
 export default FormInput;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,57 +1,48 @@
 import { Fragment, useContext } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
 
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
-import "./navigation.styles.scss";
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from "./navigation.styles";
 
 const Navigation = () => {
-  // Get the current user object from the UserContext using the useContext hook
   const { currentUser } = useContext(UserContext);
-  // Get the isCartOpen value and setIsCartOpen function from the CartContext using the useContext hook
   const { isCartOpen } = useContext(CartContext);
 
   return (
-    <>
-      {/* Use Fragment to group child elements without adding extra nodes to the DOM */}
-      <div className="navigation">
-        {/* Use Link to create a clickable link to the home page */}
-        <Link className="logo-container" to="/">
-          {/* Use an SVG file as a React component */}
+    <Fragment>
+      <NavigationContainer>
+        <LogoContainer to="/">
           <CrwnLogo className="logo" />
-        </Link>
-        {/* Group the navigation links together in a container */}
-        <div className="nav-links-container">
-          {/* Use Link to create a clickable link to the shop page */}
-          <Link className="nav-link" to="/shop">
-            SHOP
-          </Link>
-          {/* Conditionally render a "SIGN OUT" link if a user is signed in, or a "SIGN IN" link if no user is signed in */}
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to="/shop">SHOP</NavLink>
+
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
+            <NavLink as="span" onClick={signOutUser}>
               SIGN OUT
-            </span>
+            </NavLink>
           ) : (
-            <Link className="nav-link" to="/auth">
-              SIGN IN
-            </Link>
+            <NavLink to="/auth">SIGN IN</NavLink>
           )}
-          {/* Render the CartIcon component */}
           <CartIcon />
-        </div>
-        {/* Conditionally render the CartDropdown component if isCartOpen is true */}
+        </NavLinks>
         {isCartOpen && <CartDropdown />}
-      </div>
-      {/* Render any child components that match the current route using the Outlet component */}
+      </NavigationContainer>
       <Outlet />
-    </>
+    </Fragment>
   );
 };
 
