9f641aa0bffc9c255a6150f42fb4ee709e92566d
Clay Sawyer <claysawyer@outlook.com>
2023-04-19 07:39:42 -0500

    add dummy shop data

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,14 @@ import {
   signInWithEmailAndPassword,
   onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from "firebase/firestore";
 
 // Firebase configuration object
 const firebaseConfig = {
@@ -25,36 +32,70 @@ const firebaseConfig = {
 // Initialize Firebase with the configuration object
 const firebaseApp = initializeApp(firebaseConfig);
 
+// Create a new GoogleAuthProvider instance
 const googleProvider = new GoogleAuthProvider();
 
+// Set the custom parameters for the GoogleAuthProvider
 googleProvider.setCustomParameters({
   prompt: "select_account",
 });
 
+// Get the auth and firestore instances from the Firebase app instance
 export const auth = getAuth();
+export const db = getFirestore();
+
+// Google Popup and Redirect
 export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () =>
   signInWithRedirect(auth, googleProvider);
 
-export const db = getFirestore();
+// Function to add a collection and documents to the firestore database
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  // Create a new batch instance
+  const batch = writeBatch(db);
 
-// Function to create a user document in Firestore from an authentication object
+  // Get a reference to the collection
+  const collectionRef = collection(db, collectionKey);
+
+  // Loop through each object and add it to the batch
+  objectsToAdd.forEach((object) => {
+    // Create a new document reference for the object
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+
+    // Add the object to the batch
+    batch.set(docRef, object);
+  });
+
+  // Commit the batch to the firestore database
+  await batch.commit();
+  console.log("done");
+};
+
+// Function to create a user document in the users collection when a user logs in
 export const createUserDocumentFromAuth = async (
   userAuth,
   additionalInformation = {}
 ) => {
+  // Check if the user is authenticated
   if (!userAuth) return;
 
+  // Get a reference to the user document
   const userDocRef = doc(db, "users", userAuth.uid);
 
+  // Get the user snapshot
   const userSnapshot = await getDoc(userDocRef);
 
+  // If the user document doesn't exist, create a new one
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
+      // Set the user document with the user information
       await setDoc(userDocRef, {
         displayName,
         email,
@@ -66,22 +107,29 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
+  // Return the user document reference
   return userDocRef;
 };
 
+// Function to create a new user account with email and password
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  // check if email and password are present
   if (!email || !password) return;
 
+  // create a user with the given email and password
   return await createUserWithEmailAndPassword(auth, email, password);
 };
 
 export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  // check if email and password are present
   if (!email || !password) return;
 
+  // sign in the user with the given email and password
   return await signInWithEmailAndPassword(auth, email, password);
 };
 
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
+  // listen to changes in the authentication state and call the callback function
   onAuthStateChanged(auth, callback);
